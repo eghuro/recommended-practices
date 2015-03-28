2903cf1f070e527115b81db2800ebe06eb1b0838
Alexandr Mansurov <alexander.mansurov@gmail.com>
2015-03-28 18:26:19 +0100

    Ukol 2 - naznak API

---
+++ ukol2/Getopt/src/Getopt.java
@@ -0,0 +1,15 @@
+/**
+ * Zakladni verejny interface knihovny
+ *
+ */
+public class Getopt {
+	/**
+	 * Uzivatel vlozi specifikaci voleb a vstupni retezec a dostane objekt s nactenymi parametry
+	 * @param input vstupni retezec
+	 * @param specification specifikace voleb
+	 * @return nactene parametry
+	 */
+	public static Storage parse(String input, Definition specification) {
+		return null;
+	}
+}
---
+++ ukol2/Getopt/src/Storage.java
@@ -0,0 +1,23 @@
+
+/**
+ * Uloziste nactenych parametru
+ *
+ */
+public class Storage {
+	
+	/**
+	 * Pristup k seznamu nactenych voleb
+	 * @return pole nactenych voleb
+	 */
+	public LoadedOption[] getLoadedOptions() {
+		return null;
+	}
+	
+	/**
+	 * Pristup k hodnotam obycejnych argumentu
+	 * @return pole nactenych argumentu
+	 */
+	public String[] getArguments() {
+		return null;
+	}
+}
